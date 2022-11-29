d4fa0c7428622d2781533fe2bd0c92178a51a96d
Haris Leghari <harisleghari61@gmail.com>
2022-11-29 22:03:49 +0500

    Update

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,12 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import "../app.css";
 
 const ToDoList = () => {
 
+  //Hooks
   const [input, setInput] = useState();
   const [items, setItems] = useState([]);
   const [mood, setMood] = useState({ backgroundColor: "black", minHeight: "100vh", overflow: "hidden" });
 
+  //Date
+  const today = new Date();
+    const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  }
+    const day = today.toLocaleDateString('en-US', options);
 
+  //Print
   const print = () => {
     if (input) {
       let newItem = [...items, input];
@@ -15,7 +26,8 @@ const ToDoList = () => {
     }
   };
 
-  function moodChange() {
+  //Mood Dark or White
+  const moodChange = () => {
     if (mood.backgroundColor === "black") {
       setMood({ backgroundColor: "white", minHeight: "100vh", overflow: "hidden" })
     } else {
@@ -23,46 +35,49 @@ const ToDoList = () => {
     }
   };
 
-  function deleteItem(id) {
+  //Delete single item
+  const deleteItem = (id) => {
     const update = items.filter((data, index) => {
       return index !== id;
     })
     setItems(update);
   };
 
-  function deleteAll() {
-    setItems([])
-  };
+  //Delete all items
+  const deleteAll = () => setItems([]);
 
+    
+
+  // Main return of todolist
   return (
     <div className="container-fluid p-5" style={mood}>
-      <div className="mood">
+      <div>
         {
           mood.backgroundColor === "black" ?
-            <img className='float-end ' src="./images/moon.png" alt="" style={{ width: "2rem" }} onClick={moodChange} />
-            : <img className='float-end ' src="./images/sun.png" alt="" style={{ width: "2rem" }} onClick={moodChange} />
+            <img className='mx-auto d-block ' src="./images/moon.png" alt="" style={{ width: "2.5rem" }} onClick={moodChange} />
+            : <img className='mx-auto d-block ' src="./images/sun.png" alt="" style={{ width: "2.5rem" }} onClick={moodChange} />
         }
       </div>
 
       <div className="container pt-5 text-center" >
         <h2 className='fw-bold fs-1 pt-4' style={{
           color: "#ff7000"
-        }}>Todo list</h2>
+        }}>{day}</h2>
 
 
-        <input style={{ width: "14rem" }} className='my-5 bg-dark shadow-lg border border-0 p-2 rounded text-white' placeholder='Enter your tast' type="text" value={input} onChange={e => setInput(e.target.value)} />
-        <button className='my-5 fw-bold  shadow-lg border border-0 px-4 py-2 rounded ms-2 ' type="submit" onClick={print} style={{ backgroundColor: "#A8DCFA" }} >Add </button>
+        <input  className=' bg-dark' placeholder='Add new list' type="text" value={input} onChange={e => setInput(e.target.value)} />
+        <button className='add' type="submit" onClick={print} >+</button>
 
 
         {items.length > 0 ? (
           <>
-            <div className='m-auto rounded  shadow-lg' style={{ width: "26rem", border: mood.backgroundColor === "black" ? "2px solid white" : "2px solid black" }}>
+            <div className='m-auto rounded  shadow-lg' style={{ width: "26rem", border: "none"}}>
               {items.map((data, index) => {
 
                 return (
 
                   <div className='' key={index}>
-                    <p className='d-inline  float-start ps-3 py-3 ' style={{ color: mood.backgroundColor === "black" ? "white" : "black" }} >{data}  </p>
+                    <p className='d-inline  float-start ps-3 py-3 ' style={{ fontSize: "26px",color: mood.backgroundColor === "black" ? "white" : "black" }} >{data}  </p>
                     <img onClick={() => { deleteItem(index) }} className='d-inline float-end pe-3 py-3' src="./images/delete.png" alt="" style={{ width: "2.5rem" }} />
 
                     <hr style={{ border: mood.backgroundColor === "black" ? "1px solid white" : "1px solid black", width: "24rem", margin: "auto" }} />
@@ -70,12 +85,12 @@ const ToDoList = () => {
                 )
               })}
             </div>
-            <button className='mt-5 fw-bold py-2 px-3 bg-dark text-danger' onClick={deleteAll}>Delete All</button>
+            <button className='mt-5 fw-bold py-2 px-4 bg-dark text-danger rounded' onClick={deleteAll}>Delete Everything</button>
           </>
         )
           :
           <div>
-            <img src="./images/TODo list.svg" alt="" style={{ width: "22rem" }} />
+            <img src="./images/todo-list.png" alt="" style={{ width: "22rem" }} />
           </div>}
 
 
